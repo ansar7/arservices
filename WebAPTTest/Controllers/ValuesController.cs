221744696c6e6b8992d9034d2253dab94b08aa2b
ansar7 <ansar.iqbal7@gmail.com>
2013-11-05 20:26:05 +0500

    Ar service for final year

---
+++ WebAPTTest/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPTTest.Models;
+using WebAPTTest.Services;
+
+namespace WebAPTTest.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values?log=71&lat=72&azu=1.2
+        
+        public ARDATA[] Get(double log,double lat,double azu)
+        {
+            ARservice ars = new ARservice();
+            ARDATA[] ar=ars.GetContacts(log,lat,azu);
+            return ar;
+        }
+        // GET api/values
+        public ARDATA[] Get()
+        {
+            ARservice ars = new ARservice();
+            ARDATA[] ar=ars.GetAllContacts();
+            return ar;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebAPTTest/Services/ARservice.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPTTest.Models;
+
+
+namespace WebAPTTest.Services
+{
+    public class ARservice
+    {
+       
+        public ARDATA[] GetAllContacts()
+        {
+
+            ARDATA[] ar = new ARDATA[] { };
+
+            var cx = new ARDBEntities2();
+   
+             ar = cx.ARDATAs.Select(x => x).ToArray();
+
+            return ar;
+
+        }
+        public ARDATA[] GetContacts(double rlog,double rlat,double raz)
+        {
+
+            ARDATA[] ar = new ARDATA[] { };
+
+            var cx = new ARDBEntities2();
+
+            ar = cx.ARDATAs.Where(x => (x.Longitute==rlog && x.Latitude==rlat && x.Azimuth==raz)).Select(x => x).ToArray();
+            
+            //var query = cx.ARDATAs.Where(u => u.Longitute == rlog ).First();
+            
+            
+            return ar;
+
+
+
+        }
+
+    }
+}
